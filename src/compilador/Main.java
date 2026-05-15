c1700bd7a74ac3ea354f9fe3096e572a1c1bff6b
Anabsouslima1 <anaecarlos70@gmail.com>
2026-05-15 14:09:19 -0300

    Reorganizando a Main

--- src/compilador/Main.java
+++ src/compilador/Main.java
@@ -17,10 +17,7 @@ public class Main {
             System.out.println("╚══════════════════════════════════════════╝");
             System.out.println("Digite seu código T-minus linha por linha.");
             System.out.println("Digite FIM para gerar Pascal.");
-            System.out.println("Digite TREE para imprimir a AST.");
             System.out.println("──────────────────────────────────────────");
-
-            boolean mostrarArvore = false;
 
             while (scanner.hasNextLine()) {
 
@@ -30,34 +27,31 @@ public class Main {
                     break;
                 }
 
-                if (linha.trim().equalsIgnoreCase("TREE")) {
-                    mostrarArvore = true;
-                    break;
-                }
-
                 codigo.append(linha).append("\n");
             }
 
             System.out.println("──────────────────────────────────────────");
 
             try {
-
                 List<Token> tokens = new Lexer(codigo.toString()).getTokens();
                 Node raiz = new Parser(tokens).parseProg();
 
-                if (mostrarArvore) {
+                System.out.println("PASCAL GERADO:");
+                System.out.println("──────────────────────────────────────────");
 
-                    System.out.println("ÁRVORE SINTÁTICA (AST)");
+                String pascal = new Gerador().gerar(raiz);
+                System.out.println(pascal);
+
+                System.out.println("\nDeseja visualizar a AST? (S/N)");
+
+                String arvore = scanner.nextLine();
+
+                if (arvore.equalsIgnoreCase("S")) {
+
+                    System.out.println("\nÁRVORE SINTÁTICA (AST)");
                     System.out.println("──────────────────────────────────────────");
+
                     raiz.imprimir();
-
-                } else {
-
-                    System.out.println("PASCAL GERADO:");
-                    System.out.println("──────────────────────────────────────────");
-
-                    String pascal = new Gerador().gerar(raiz);
-                    System.out.println(pascal);
                 }
 
             } catch (Exception e) {
@@ -69,7 +63,7 @@ public class Main {
             String resposta = scanner.nextLine();
 
             if (!resposta.equalsIgnoreCase("S")) {
-                System.out.println("Encerrando compilador...");
+                System.out.println("Encerrando compilador... Obrigado por usar o T-Minus!");
                 break;
             }
         }
