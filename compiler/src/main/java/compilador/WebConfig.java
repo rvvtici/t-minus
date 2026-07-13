492f001f7bc0da204925e5eabd808082b045ec19
ravi <rvvtici@gmail.com>
2026-07-13 14:28:27 -0300

    Dockerfile

--- compiler/src/main/java/compilador/WebConfig.java
+++ compiler/src/main/java/compilador/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000") // origem do seu Next.js local
+                .allowedOrigins("https://rvvtici.vercel.app/") // origem do seu Next.js local
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
